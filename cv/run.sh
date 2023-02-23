51fb0f1e5b6ad229097e190689dd93134d0a7db4
ᨀ Max ᨀ <57547869+mahendraplus@users.noreply.github.com>
2023-02-24 00:57:04 +0545

    Add files via upload

---
+++ cv/run.sh
@@ -0,0 +1,4 @@
+sudo apt install wget curl 
+wget https://raw.githubusercontent.com/mahendraplus/Mahendraplus/main/cv/max.sh
+wget https://raw.githubusercontent.com/mahendraplus/Mahendraplus/main/cv/a
+bash max.sh
