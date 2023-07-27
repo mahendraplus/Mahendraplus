2345d9b5ef07de7b81011688d2b7154b8ce8af2c
ᨀ Max ᨀ <57547869+mahendraplus@users.noreply.github.com>
2023-07-27 15:46:04 +0530

    Delete cv directory

--- cv/run.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install wget curl 
-wget https://raw.githubusercontent.com/mahendraplus/Mahendraplus/main/cv/max.sh
-wget https://raw.githubusercontent.com/mahendraplus/Mahendraplus/main/cv/a
-bash max.sh
